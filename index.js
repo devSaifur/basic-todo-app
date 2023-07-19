6508c216190d86c9c26c86b6db16be177100e5b3
Saifur Rahman <118433585+devSaifur@users.noreply.github.com>
2023-07-19 21:41:25 +0600

    fix code formatting

--- index.js
+++ index.js
@@ -1,121 +1,122 @@
-require('dotenv').config()
-const express = require("express")
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const { name } = require("ejs")
-const _ = require("lodash")
-const date = require(__dirname + "/date.js")
-const PORT = process.env.PORT || 3000
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { name } = require("ejs");
+const _ = require("lodash");
+const date = require(__dirname + "/date.js");
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static("public"))
-app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-const day = date.getDay()
+const day = date.getDay();
 
 const itemSchema = {
-    name: String
-}
+  name: String,
+};
 
-const Item = mongoose.model("Item", itemSchema)
+const Item = mongoose.model("Item", itemSchema);
 
-const item1 = new Item({name: "Welcome to your todolist"})
-const item2 = new Item({name: "Hit the + button to add a new item"})
-const item3 = new Item({name: "<-- Hit this to delete an item"})
+const item1 = new Item({ name: "Welcome to your todolist" });
+const item2 = new Item({ name: "Hit the + button to add a new item" });
+const item3 = new Item({ name: "<-- Hit this to delete an item" });
 
-const defaultItems = [item1, item2, item3]
+const defaultItems = [item1, item2, item3];
 
 const listSchema = {
-    name: String,
-    items: [itemSchema]
-}
+  name: String,
+  items: [itemSchema],
+};
 
-const List = mongoose.model("List", listSchema)
+const List = mongoose.model("List", listSchema);
 
 app.get("/", (req, res) => {
-    Item.find({})
-        .then((foundItems) => {
-            if (foundItems.length === 0) {
-                Item.insertMany(defaultItems)
-                res.redirect("/")
-            } else {
-                res.render("list", {listTitle: day , newListItems: foundItems})
-            }
-        })
-})
+  Item.find({}).then((foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems);
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: day, newListItems: foundItems });
+    }
+  });
+});
 
 app.post("/", (req, res) => {
-    const itemName = req.body.newItem
-    const listName = req.body.list
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-    const itemInput = new Item({name: itemName})
-    
-    if (listName === day) {
-        itemInput.save()
-        res.redirect("/")
-    } else {
-        List.findOne({name: listName})
-            .then((foundList) => {
-                foundList.items.push(itemInput)
-                foundList.save()
-                res.redirect("/" + listName)
-            })
-    }
-})
+  const itemInput = new Item({ name: itemName });
+
+  if (listName === day) {
+    itemInput.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }).then((foundList) => {
+      foundList.items.push(itemInput);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
 
 app.post("/delete", (req, res) => {
-    const chackedItemId = req.body.checkbox
-    const listName = req.body.listName.trim()
+  const chackedItemId = req.body.checkbox;
+  const listName = req.body.listName.trim();
 
-    if (listName === day) {
-        Item.findByIdAndRemove(chackedItemId)
-            .then(() => {
-                res.redirect("/")
-            })
-    } else {
-        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: chackedItemId}}})
-            .then(() => {
-                res.redirect("/" + listName)
-            })
-    }   
-})
+  if (listName === day) {
+    Item.findByIdAndRemove(chackedItemId).then(() => {
+      res.redirect("/");
+    });
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: chackedItemId } } }
+    ).then(() => {
+      res.redirect("/" + listName);
+    });
+  }
+});
 
 app.get("/:routeName", (req, res) => {
-    const customListName = _.capitalize(req.params.routeName)
+  const customListName = _.capitalize(req.params.routeName);
 
-    List.findOne({name: customListName})
-        .then((foundList) => {
-            if (!foundList) {
-                const list = new List({
-                    name: customListName,
-                    items: defaultItems
-                })
-                list.save()
-                res.redirect("/" + customListName)
-            } else {
-                res.render("list", {listTitle: foundList.name , newListItems: foundList.items})
-            }
-    })
-})
+  List.findOne({ name: customListName }).then((foundList) => {
+    if (!foundList) {
+      const list = new List({
+        name: customListName,
+        items: defaultItems,
+      });
+      list.save();
+      res.redirect("/" + customListName);
+    } else {
+      res.render("list", {
+        listTitle: foundList.name,
+        newListItems: foundList.items,
+      });
+    }
+  });
+});
 
 app.get("/about", (req, res) => {
-    res.render("about")
-})
+  res.render("about");
+});
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`surver is running on port ${PORT}`)
-    })
-})
+  app.listen(PORT, () => {
+    console.log(`surver is running on port ${PORT}`);
+  });
+});
